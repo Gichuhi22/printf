6be35998f717f8fffa19c4f65a0d6763fe870678
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-23 15:52:37 +0000

    betty update

--- printf_reverse.c
+++ printf_reverse.c
@@ -9,18 +9,18 @@ int printf_reverse(char *s)
 {
 	int length, i, j;
 	char temp;
-	
+
 	if (!s)
 		return (-1);
 
 	length = strlen(s);
-	for(i = 0, j = length; i < j; i++, j--)
+	for (i = 0, j = length; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
 		putchar(s[i]);
 	}
-	
+
 	return (length);
 }
