50eb8e264c2ad1739e4d33357d2b4e6e19294956
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-10-23 09:12:29 +0000

    modified file printf_schar.c

--- printf_schar.c
+++ printf_schar.c
@@ -9,29 +9,24 @@
 int printf_schar(char *s)
 {
 	int i;
-	char *str;
 
 	if (s == NULL)
 		return (-1);
 
-	str = malloc(sizeof(char) * (strlen(s) + 2));
 	for (i = 0; s[i] != '\0'; i++)
-		str[i] = s[i];
-
-	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
+		if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
 		{
 			_putchar(92);
 			_putchar(120);
-			if (str[i] <= 16)
+			if (s[i] <= 16)
 				_putchar ('0');
-			i += printf_hexaupper(str[i]);
-			_putchar(str[i]);
+			i += printf_hexaupper(s[i]);
+			_putchar(s[i]);
 		}
 		else
 		{
-			_putchar(str[i]);
+			_putchar(s[i]);
 		}
 	}
 	return (i);
